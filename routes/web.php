092ed2b8bf33de451c15e829576c69cdb887cfe6
Tawsif-Khan <tawsif.online@gmail.com>
2023-07-10 19:38:46 +0600

    talent corosal

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\LinkedinController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,10 @@ Route::get('/talent', function () {
     return view('pages.index');
 })->name('talent.home');
 Route::get('/', function () {
-    return view('pages.business');
+
+    $publishedTalents = User::with('talent')->where('role', 'Talent')->get();
+
+    return view('pages.business')->with('talents', $publishedTalents);
 })->name('home');
 Route::get('/privacy-policy', function () {
     return view('pages.privacy');
@@ -36,22 +40,22 @@ Route::get('/job-details', 'App\http\controllers\JobController@jobDetail')->name
 Route::get('/view-profile/{id}', 'App\http\controllers\TalentProfileController@show')->name('show.profile');
 Route::get('/build-profile', 'App\http\controllers\TalentProfileController@index')->name('build.profile');
 Route::get('/talent-care', 'App\http\controllers\JobController@talentCare')->name('talent.care');
-Route::post('/submit-profile','App\http\controllers\TalentProfileController@store')->name('submit.profile');
-Route::post('/submit-contract','App\http\controllers\ContractController@store')->name('submit.contract');
-Route::post('/end-contract','App\http\controllers\ContractController@endContract')->name('end.contract');
-Route::post('/add-experience','App\http\controllers\TalentProfileController@addExperience')->name('add.experience');
-Route::post('/add-education','App\http\controllers\TalentProfileController@addEducation')->name('add.education');
-Route::post('/accept-request','App\http\controllers\JobController@acceptRequest')->name('accept.request');
-Route::post('/reject-request','App\http\controllers\JobController@rejectRequest')->name('reject.request');
+Route::post('/submit-profile', 'App\http\controllers\TalentProfileController@store')->name('submit.profile');
+Route::post('/submit-contract', 'App\http\controllers\ContractController@store')->name('submit.contract');
+Route::post('/end-contract', 'App\http\controllers\ContractController@endContract')->name('end.contract');
+Route::post('/add-experience', 'App\http\controllers\TalentProfileController@addExperience')->name('add.experience');
+Route::post('/add-education', 'App\http\controllers\TalentProfileController@addEducation')->name('add.education');
+Route::post('/accept-request', 'App\http\controllers\JobController@acceptRequest')->name('accept.request');
+Route::post('/reject-request', 'App\http\controllers\JobController@rejectRequest')->name('reject.request');
 Route::get('/client-view', 'App\http\controllers\TalentProfileController@showTalentProfile')->name('client.view');
 Route::post('/submit-for-review', 'App\http\controllers\TalentProfileController@submitForReview')->name('submit.for.review');
 
 Route::post('/update-hourly-rate', 'App\http\controllers\TalentProfileController@updateHourlyRate')->name('updateHourlyRate');
-Route::post('/update-hours-per-week','App\http\controllers\TalentProfileController@updateHoursPerWeek')->name('updateHoursPerWeek');
-Route::post('/update-ex-famous-company','App\http\controllers\TalentProfileController@updateExFamousCompany')->name('updateExFamousCompany');
-Route::post('/update-bio','App\http\controllers\TalentProfileController@updateBio')->name('updateBio');
-Route::post('/update-experience','App\http\controllers\TalentProfileController@updateExp')->name('updateExp');
-Route::post('/update-education','App\http\controllers\TalentProfileController@updateEdu')->name('updateEdu');
+Route::post('/update-hours-per-week', 'App\http\controllers\TalentProfileController@updateHoursPerWeek')->name('updateHoursPerWeek');
+Route::post('/update-ex-famous-company', 'App\http\controllers\TalentProfileController@updateExFamousCompany')->name('updateExFamousCompany');
+Route::post('/update-bio', 'App\http\controllers\TalentProfileController@updateBio')->name('updateBio');
+Route::post('/update-experience', 'App\http\controllers\TalentProfileController@updateExp')->name('updateExp');
+Route::post('/update-education', 'App\http\controllers\TalentProfileController@updateEdu')->name('updateEdu');
 
 Route::get('/assessment/{category_id}', 'App\http\controllers\AssesmentController@assessment')->name('assessment.init');
 Route::get('/quiz-running/{category_id}', 'App\http\controllers\AssesmentController@generateQuestions')->name('assessment.running');
@@ -61,26 +65,26 @@ Route::get('/results', 'App\http\controllers\AssesmentController@results')->name
 
 // chat routes
 
-Route::post('/send-message','App\http\controllers\ChatController@sendMessage')->name('send.message');
+Route::post('/send-message', 'App\http\controllers\ChatController@sendMessage')->name('send.message');
 Route::post('/upload-chat-file', 'App\http\controllers\ChatController@uploadChatFile')->name('upload.chat.file');
 Route::get('/download-chat-file/{file_id}', 'App\http\controllers\ChatController@downloadFile')->name('download.chat.file');
 
 
-Route::get('/redirect/admin/{id}','App\http\controllers\Client\AuthController@redirectToAdmin')->name('redirectToAdmin');
+Route::get('/redirect/admin/{id}', 'App\http\controllers\Client\AuthController@redirectToAdmin')->name('redirectToAdmin');
 
 Route::prefix('/client')->as('client.')->group(function () {
-    
-    Route::post('/auth/register','App\http\controllers\Client\AuthController@register')->name('register');    
-    Route::post('/auth/login','App\http\controllers\Client\AuthController@login')->name('login');
+
+    Route::post('/auth/register', 'App\http\controllers\Client\AuthController@register')->name('register');
+    Route::post('/auth/login', 'App\http\controllers\Client\AuthController@login')->name('login');
 });
 
-Route::prefix('/client')->as('client.')->middleware(['auth','verified'])->group(function () {
-    
-    Route::get('/dashboard','App\http\controllers\Client\JobController@jobsPage')->name('dashboard');   
-    Route::get('/job-request/new','App\http\controllers\Client\JobController@create')->name('job.new'); 
-    Route::post('/job-request/create','App\http\controllers\Client\JobController@store')->name('job.create'); 
-    Route::get('/job-details/{id}','App\http\controllers\Client\JobController@jobDetails')->name('job.details'); 
-    Route::get('/job-detail','App\http\controllers\Client\JobController@jobDetail')->name('job.detail');    
+Route::prefix('/client')->as('client.')->middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('/dashboard', 'App\http\controllers\Client\JobController@jobsPage')->name('dashboard');
+    Route::get('/job-request/new', 'App\http\controllers\Client\JobController@create')->name('job.new');
+    Route::post('/job-request/create', 'App\http\controllers\Client\JobController@store')->name('job.create');
+    Route::get('/job-details/{id}', 'App\http\controllers\Client\JobController@jobDetails')->name('job.details');
+    Route::get('/job-detail', 'App\http\controllers\Client\JobController@jobDetail')->name('job.detail');
     Route::get('/view-talent-profile/{id}', 'App\http\controllers\Client\JobController@showTalentProfile')->name('show.profile');
     Route::post('/request-invoice', 'App\http\controllers\Client\JobController@requestInvoice')->name('requestInvoice');
     Route::post('/approve-deposit', 'App\http\controllers\Client\JobController@approveDeposit')->name('approveDeposit');
@@ -88,37 +92,36 @@ Route::prefix('/client')->as('client.')->middleware(['auth','verified'])->group(
 
 
 Route::domain('admin.' . env('APP_URL'))->middleware('auth')->group(function () {
-    
+
     require __DIR__ . '/admin.php';
 });
 
 Route::prefix('/admin')->middleware('auth')->group(function () {
-    
-    
+
+
     require __DIR__ . '/admin.php';
 });
 
 Route::domain('admin.' . env('APP_URL'))->group(function () {
-    
-    Route::get('/login','App\http\controllers\Admin\AuthController@index')->name('admin.login.form');
-    Route::post('/auth/login','App\http\controllers\Admin\AuthController@login')->name('admin.login');
+
+    Route::get('/login', 'App\http\controllers\Admin\AuthController@index')->name('admin.login.form');
+    Route::post('/auth/login', 'App\http\controllers\Admin\AuthController@login')->name('admin.login');
 });
 
 Route::prefix('/admin')->group(function () {
-  
-    Route::get('/login','App\http\controllers\Admin\AuthController@index')->name('admin.login.form');
-    Route::post('/auth/login','App\http\controllers\Admin\AuthController@login')->name('admin.login');
+
+    Route::get('/login', 'App\http\controllers\Admin\AuthController@index')->name('admin.login.form');
+    Route::post('/auth/login', 'App\http\controllers\Admin\AuthController@login')->name('admin.login');
 });
 
-Route::group(['middleware' => ['auth']], function() {
-    
+Route::group(['middleware' => ['auth']], function () {
+
     /**
-    * Verification Routes
-    */
+     * Verification Routes
+     */
     // Route::get('/email/verify', 'App\http\controllers\Email\VerificationController@show')->name('verification.notice');
     Route::get('/email/verify/{id}/{hash}', 'App\http\controllers\Email\VerificationController@verify')->name('verification.verify')->middleware(['signed']);
     Route::post('/email/resend', 'App\http\controllers\Email\VerificationController@resend')->name('verification.resend');
-  
 });
 
 Route::get('/email/verify', function () {
@@ -126,10 +129,10 @@ Route::get('/email/verify', function () {
 })->middleware('auth')->name('verification.notice');
 //Feedback
 
-Route::post('feedback','App\http\Controllers\FeedbackController@store')->name('feedback.store');
-Route::post('email/isexist','App\http\Controllers\Client\AuthController@isEmailExist')->name('email.isexist');
+Route::post('feedback', 'App\http\Controllers\FeedbackController@store')->name('feedback.store');
+Route::post('email/isexist', 'App\http\Controllers\Client\AuthController@isEmailExist')->name('email.isexist');
 
- 
+
 Route::get('auth/linkedin', [LinkedinController::class, 'linkedinRedirect']);
 Route::get('auth/linkedin/callback', [LinkedinController::class, 'linkedinCallback']);
 require __DIR__ . '/auth.php';
