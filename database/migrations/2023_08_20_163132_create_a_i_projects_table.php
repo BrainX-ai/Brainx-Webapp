21e1cacc8832d1b2c13b66989cbe446f313870fd
Tawsif-Khan <tawsif.online@gmail.com>
2023-08-20 22:36:21 +0600

    AI project migrations

---
+++ database/migrations/2023_08_20_163132_create_a_i_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('a_i_projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->text('title')->nullable();
+            $table->text('description')->nullable();
+            $table->double('budget')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('a_i_projects');
+    }
+};
