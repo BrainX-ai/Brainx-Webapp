1bdb551638856c3715055a761ccd54ca4d70c3fc
Tawsif-Khan <tawsif.online@gmail.com>
2023-08-21 18:31:24 +0600

    bubble profile

--- resources/views/livewire/profile/tabs.blade.php
+++ resources/views/livewire/profile/tabs.blade.php
@@ -1,6 +1,6 @@
-<section class="great-about container mb-3">
+<section class=" container mb-3">
 
-    <div class="col-md-12 mt-4 ">
+    <div class="col-md-12  ">
         @foreach ($tabs as $key => $tab)
             <label wire:key="{{ $key }}" for="{{ $key }}"
                 class="btn btn-sm rounded-pill  @if ($tab == $activeTab) btn-primary @else btn-outline-primary @endif m-1 ps-3 pe-3  shadow">
